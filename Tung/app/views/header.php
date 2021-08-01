d05d5ef4e080629c2b3fca9d80c1efb492ac37b2
TungUtas <87627454+TungUtas@users.noreply.github.com>
2021-08-02 09:54:30 +1000

    tung 02/08

---
+++ Tung/app/views/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="./app/assets/img/apple-icon.png"
+    />
+    <link rel="icon" type="image/png" href="./app/assets/img/favicon.png"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Career Portal</title>
+    <meta
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no"
+            name="viewport"
+    />
+    <!--     Fonts and icons     -->
+    <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200"
+            rel="stylesheet"
+    />
+    <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+            integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+            crossorigin="anonymous"
+    />
+    <!-- CSS Files -->
+    <link href="./app/assets/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="./app/assets/css/theme.css" rel="stylesheet"/>
+    <!--   Core JS Files   -->
+    <script src="./app/assets/js/core/jquery.min.js"></script>
+    <script src="./app/assets/js/core/popper.min.js"></script>
+    <script src="./app/assets/js/core/bootstrap.min.js"></script>
+    <!-- Forms Validations Plugin -->
+    <script src="./app/assets/js/plugins/jquery.validate.min.js"></script>
+    <!--  Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+    <script src="./app/assets/js/plugins/jquery.bootstrap-wizard.js"></script>
+    <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+    <script src="./app/assets/js/plugins/bootstrap-selectpicker.js"></script>
+    <script src="./app/assets/demo/demo.js"></script>
+</head>
+<body class="sidebar-mini">
+<div class="wrapper">
