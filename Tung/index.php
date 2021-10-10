a126cda2fd6acf9afe66c7855a2923b232f6bb8d
NickMeng999 <87627462+NickMeng999@users.noreply.github.com>
2021-10-11 09:32:21 +1100

    1

--- Tung/index.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-// 	First file to run by default
-
-	require_once("connection.php"); // establish connection to database 
-	$ctr = isset($_GET['ctr']) == false ? "Student" : $_GET['ctr'];
-	$action = isset($_GET['action']) == false ? "login" : $_GET['action'];
-
-	include './app/views/layout.php';
-?>
--- Tung/readme.txt
+++
@@ -1,6 +0,0 @@
-Student Account: 
--thanhtung123	/ 123456
-Staff Account	
--thanhtungadmin / 123456
-SuperAdmin:
-- admin /123456
